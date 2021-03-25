c0d5b01f9a98c5a3844a19d2017a600b70879b0e
sadekeen20 <udoy320@gmail.com>
2021-03-25 19:02:11 +0600

    firebase syntax changed

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 // Firebase App (the core Firebase SDK) is always required and must be listed first
-import * as firebase from "firebase/app";
+// import * as firebase from "firebase/app";
+import  firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
 import { useHistory, useLocation } from 'react-router-dom';
